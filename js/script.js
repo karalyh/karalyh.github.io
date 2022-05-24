189b600e18550c9bedbf9f66f17814db12588f3d
root <root@labreact.experts-ci.com.novalocal>
2022-05-24 01:27:33 +0000

    update

--- js/script.js
+++ js/script.js
@@ -7,7 +7,16 @@ let menu = {
 function loadMenu(){
 	//sidebar=document.querySelector('#sidebar');
 	console.log(menu);
+	menu.forEach(createMenu);
 	
+}
+function createMenu(libelle){
+	const li=document.createElement("li");
+	const litext=document.createTextNode(libelle);
+	const mprinc=document.getElementById("menu");
+	li.appendChild(litext);
+	mprinc.appendChild(li);
+
 }
 				
 	
